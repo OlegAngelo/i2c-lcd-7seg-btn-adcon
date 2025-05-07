68a5df395e808b66dc1f1c066d9d690bd518b89b
OlegAngelo <naubalagtas@gmail.com>
2025-05-07 20:16:17 +0800

    add rbo/int function, uart function, and 7 segment display for data

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 #pragma config CP = OFF
 
 float voltageOutput = 0;
-unsigned int wholeNum, decimalNum, tempNum;
+unsigned int wholeNum, decimalNum;
 int d_value = 0;
 char data[20];
 
@@ -24,12 +24,6 @@ void delay(int time)
 	}
 }
 
-void portConfig(void) {
-    TRISD = 0x00; // data for LCD
-	TRISB = 0x00; // instructions for LCD
-}
-
-
 void instCtrl(unsigned char data)
 {
 	PORTD = data; // load data to port c since my port c is the output pins
@@ -40,7 +34,7 @@ void instCtrl(unsigned char data)
 	RB7 = 0; // set E to 0 (strobe)
 }
 
-void initLCD()
+void lcdConfig()
 {
 	delay(50);
 
@@ -48,7 +42,7 @@ void initLCD()
 	instCtrl(0x38); // display off
 	instCtrl(0x01); // display clear
 	instCtrl(0x06); // entry mode: increment; shift off
-	instCtrl(0x0E); // display on; cursor off; blink off
+	instCtrl(0x0C); // display on; cursor off; blink off
 }
 
 void dataCtrl(unsigned char data)
@@ -61,28 +55,14 @@ void dataCtrl(unsigned char data)
 	RB7 = 0; // set E to 0 (strobe)
 }
 
-void adcConfig(void) {
-    // configure for ADC
-    ADCON1 = 0x80; // result register: right Justified, clock: FOSC/8
-                   // all ports in PORTA are analog
-                   // VREF+=VDD, VREF-=VSS
-    ADCON0 = 0x41; // clock: FOSC/8 analog channel: AN0
-                   // A/D conversion: STOP, A/D module: ON
-    ADIE = 1;      // A/D conversion complete interrupt enable (PIE1 reg)
-    ADIF = 0;      // reset interrupt flag (PIR1 reg)
-    PEIE = 1;      // enable all peripheral interrupt (INTCON reg)
-    GO = 1;        // start A/D conversion (ADCON0 reg)
-    GIE = 1;       // enable all unmasked interrupts (INTCON reg)
-}
-
 void interrupt ISR(void)
 {
-	char voltage[] = "VOLTAGE:";
-
     GIE = 0; // disable all unmasked interrupts (INTCON reg)
 
     if (ADIF == 1) // checks CCP1 interrupt flag
     {
+		char voltage[] = "VOLTAGE:";
+
         ADIF = 0; // clears interrupt flag (INTCON reg)
 		
         d_value = ((ADRESH << 8) + ADRESL); // read ADRESH, move to correct position, read ADRESL
@@ -100,19 +80,77 @@ void interrupt ISR(void)
 		instCtrl(0xC0);
 		sprintf(data, "%d.%d ", wholeNum, decimalNum);
 		for (char *p = data; *p; p++) dataCtrl(*p);
+
+    	delay(100); // delay to get the hold capacitor charged
+    	GO = 1; // restart A/D conversion (ADCON0 reg)
     }
 
-    delay(100); // delay to get the hold capacitor charged
-    GO = 1; // restart A/D conversion (ADCON0 reg)
+	// intf needs to be outside so it doesnt depend on the pot to be changed
+	// in order to transfer data
+	if (INTF) { // check rb0 interrupt
+		INTF = 0;
+		RC0 ^= 1; // led to test if interrupt rb0 is working
+
+		unsigned char transmitData = (wholeNum << 4) | (decimalNum & 0x0F);
+
+		while(!TRMT);
+		TXREG = transmitData;
+	}
+
+
     GIE = 1; // enable all unmasked interrupts (INTCON reg)
 }
 
+void interruptConfig(void) {
+	OPTION_REG = 0xC4; // 1100 0100
+	INTE = 1; // int enable
+	INTF = 0; // int flag clear
+}
+
+void adcConfig(void) {
+    // configure for ADC
+    ADCON1 = 0x80; // result register: right Justified, clock: FOSC/8
+                   // all ports in PORTA are analog
+                   // VREF+=VDD, VREF-=VSS
+    ADCON0 = 0x41; // clock: FOSC/8 analog channel: AN0
+                   // A/D conversion: STOP, A/D module: ON
+    ADIE = 1;      // A/D conversion complete interrupt enable (PIE1 reg)
+    ADIF = 0;      // reset interrupt flag (PIR1 reg)
+    GO = 1;        // start A/D conversion (ADCON0 reg)
+}
+
+void asyncTransmitterConfig(void){
+	// transmitter config
+ 	SPBRG = 0x19; // 9.6K baud rate @ FOSC=4MHz, asynchronous high speed
+ 			      // (see formula in Table 10-1 data sheet)
+ 	SYNC = 0; // asynchronous mode (TXSTA reg)
+ 	SPEN = 1; // enable serial port (RCSTA reg)
+ 	TX9 = 0; // 8-bit transmission (TXSTA reg)
+ 	BRGH = 1; // asynchronous high-speed (TXSTA reg)
+ 	TXEN = 1; // transmit enable (TXSTA reg)
+}
+
+void portsConfig(void) {
+    TRISD = 0x00; // data for LCD
+	TRISB = 0x01; // instructions for LCD & rb0 as input for button
+	TRISC = 0x00; // output to led to test rb0 int
+				  // output to mcu2 (master)
+}
 
 void main(void)
 {
-    portConfig();
-	initLCD();
+    portsConfig();
+	lcdConfig();
    	adcConfig();
+
+	asyncTransmitterConfig();
+
+	interruptConfig(); // rb0 int
+
+	RB0 = 0; // set led to low
+	
+    PEIE = 1;      // enable all peripheral interrupt (INTCON reg)
+    GIE = 1;       // enable all unmasked interrupts (INTCON reg)
 
     while(1)
     {
---
+++ main2.c
@@ -0,0 +1,61 @@
+#include <xc.h>
+
+#pragma config FOSC = XT
+#pragma config WDTE = OFF
+#pragma config PWRTE = ON
+#pragma config BOREN = ON
+#pragma config LVP = OFF
+#pragma config CPD = OFF
+#pragma config WRT = OFF
+#pragma config CP = OFF
+
+void delay(int time)
+{
+	int i, j;
+	for (i = 0; i < time; i++)
+	{
+		for (j = 0; j < 100; j++);
+	}
+}
+
+void usart_rx_init (void){
+	// receiver config
+	SPBRG = 0x19; // 9.6K baud rate @ FOSC=4MHz, asynchronous high speed
+				// (see formula in Table 10-1 data sheet)
+	SYNC = 0; // asynchronous mode (TXSTA reg)
+	SPEN = 1; // enable serial port (RCSTA reg)
+	RX9 = 0; // 8-bit reception (TXSTA reg)
+	BRGH = 1; // asynchronous high-speed (TXSTA reg)
+	CREN = 1; // enable continuous receive (RCSTA reg)
+}
+
+void portsConfig(void) {
+    TRISB = 0x00; // output to 7 segment (wholeNum)
+    TRISD = 0x00; // output to 7 segment (decimalNum)
+    PORTB = 0; // initial to low
+    PORTD = 0; // initial to low
+
+    TRISC7 = 1; // input as uart receiver
+}
+
+void main (void) {
+    unsigned int wholeNum, decimalNum;
+    int data = 0;
+
+    usart_rx_init();
+    portsConfig();
+
+    while(1) {
+		delay (50);
+
+		while(!RCIF);
+        data = RCREG;
+
+        wholeNum = (data >> 4) & 0x0F; // extract upper nibble
+        decimalNum = data & 0x0F; // extract lower nibble
+
+        // display to 2 7 segments
+        PORTB = wholeNum;
+        PORTD = decimalNum;
+    }
+}
